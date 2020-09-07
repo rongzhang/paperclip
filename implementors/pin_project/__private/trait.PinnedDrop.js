151530f28f3b87b2ad5c54ce9a380f12738ba9e7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-07 22:22:41 +0000

    Deploy paperclip.waffles.space to github.com/wafflespeanut/paperclip.git:gh-pages

---
+++ implementors/pin_project/__private/trait.PinnedDrop.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["paperclip_core"] = [{"text":"impl&lt;T, H&gt; PinnedDrop for <a class=\"struct\" href=\"paperclip_core/v2/struct.ResponseWrapper.html\" title=\"struct paperclip_core::v2::ResponseWrapper\">ResponseWrapper</a>&lt;T, H&gt;","synthetic":false,"types":["paperclip_core::v2::actix::ResponseWrapper"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
